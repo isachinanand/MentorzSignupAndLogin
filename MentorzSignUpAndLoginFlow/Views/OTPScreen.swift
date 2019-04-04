9d16f38fcefa7c75d8576c75603a475f60df8194
sachin <saanand@craterzone.com>
2019-04-04 13:04:14 +0530

    OTP screen class made

---
+++ MentorzSignUpAndLoginFlow/Views/OTPScreen.swift
@@ -0,0 +1,29 @@
+//
+//  OTPScreen.swift
+//  MentorzSignUpAndLoginFlow
+//
+//  Created by Sachin Anand on 04/04/19.
+//  Copyright © 2019 craterzone. All rights reserved.
+//
+
+import UIKit
+
+class OTPScreen: UIViewController {
+
+    @IBOutlet weak var firstTxtField: UITextField!
+    @IBOutlet weak var secondTxtField: UITextField!
+    @IBOutlet weak var thirdTxtField: UITextField!
+    @IBOutlet weak var fourthTxtField: UITextField!
+    @IBOutlet weak var fifthTxtField: UITextField!
+    @IBOutlet weak var sixthTxtField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func verifyButtonPressed(_ sender: MentorzButton) {
+    }
+    
+
+}
